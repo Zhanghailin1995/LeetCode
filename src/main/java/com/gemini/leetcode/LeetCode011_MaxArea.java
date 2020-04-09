ee8363825f70ff04d1f519a269b3eaeaf6e3ef9b
Zhanghailin <878689660@qq.com>
2020-04-09 21:05:10 +0800

    max area bug fix

--- src/main/java/com/gemini/leetcode/LeetCode011_MaxArea.java
+++ src/main/java/com/gemini/leetcode/LeetCode011_MaxArea.java
@@ -23,7 +23,7 @@ public class LeetCode011_MaxArea {
     public int maxArea_01(int[] height) {
         int maxarea = 0, l = 0, r = height.length - 1;
         while (l < r) {
-            maxarea = Math.max(maxarea, Math.min(height[l], height[r] * (r - l)));
+            maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
             if (height[l]<height[r])
                 l++;
             else
