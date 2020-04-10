5540c91e337056da77ea89a747fcd126eea6876a
kaga <zhanghailin1995@gmail.com>
2020-04-10 17:41:43 +0800

    graph bfs dfs

---
+++ src/main/java/com/gemini/learning/Graph.java
@@ -0,0 +1,107 @@
+package com.gemini.learning;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * com.gemini.learning.Graph
+ *
+ * @author zhanghailin
+ */
+public class Graph {
+
+    private int v;// 图顶点的个数
+    private LinkedList<Integer>[] adj;// linked list 数组
+
+    public Graph(int v) {
+        this.v = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i) {
+            adj[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int s, int t) { // 无向图一条边存两次
+        adj[s].add(t);
+        adj[t].add(s);
+    }
+
+    // 广度优先搜索
+    public void bfs(int s, int t) {
+        if (s == t) return;
+        boolean[] visited = new boolean[v];// 用来记录已经被访问的顶点，用来避免顶点被重复访问
+        visited[s] = true;
+        Queue<Integer> queue = new LinkedList<>();//用来存储已经被访问、但相连的顶点还没有被访问的顶点
+        queue.add(s);
+        int[] prev = new int[v]; // 记录搜索路径
+        for (int i = 0; i < v; ++i) {
+            prev[i] = -1;
+        }
+        while (queue.size() != 0) {
+            int w = queue.poll();
+            for (int i = 0; i < adj[w].size(); ++i) {
+                int q = adj[w].get(i);
+                if (!visited[q]) {
+                    prev[q] = w;// q节点的前置节点是w
+                    if (q == t) {
+                        print(prev, s, t);
+                        return;
+                    }
+                    visited[q] = true;
+                    queue.add(q);
+                }
+            }
+        }
+    }
+
+    boolean found = false;
+
+    public void dfs(int s, int t) {
+        found = false;
+        boolean[] visited = new boolean[v];
+        int[] prev = new int[v];
+        for (int i = 0; i < v; ++i) {
+            prev[i] = -1;
+        }
+        recurDfs(s, t, visited, prev);
+        print(prev, s, t);
+    }
+
+    private void recurDfs(int w, int t, boolean[] visited, int[] prev) {
+        if (found) return;
+        visited[w] = true;
+        if (w == t) {
+            found = true;
+            return;
+        }
+        for (int i = 0; i < adj[w].size(); ++i) {
+            int q = adj[w].get(i);
+            if (!visited[q]) {
+                prev[q] = w;
+                recurDfs(q, t, visited, prev);
+            }
+        }
+    }
+
+    private void print(int[] prev, int s, int t) { // 递归打印s->t的路径
+        if (prev[t] != -1 && t != s) {
+            print(prev, s, prev[t]);
+        }
+        System.out.print(t + " ");
+    }
+
+    public static void main(String[] args) {
+        Graph graph = new Graph(8);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 3);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 4);
+        graph.addEdge(3, 4);
+        graph.addEdge(2, 5);
+        graph.addEdge(4, 5);
+        graph.addEdge(4, 6);
+        graph.addEdge(5, 7);
+        graph.addEdge(6, 7);
+        graph.dfs(0, 7);
+    }
+}
