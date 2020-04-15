6568f476731e672f874f4bc8e3f2cef65a00816a
kaga <zhanghailin1995@gmail.com>
2020-04-15 14:14:36 +0800

    generate parenthesis solution commit

---
+++ src/main/java/com/gemini/leetcode/LeetCode022_GenerateParenthesis.java
@@ -0,0 +1,88 @@
+package com.gemini.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * com.gemini.leetcode.LeetCode022_GenerateParenthesis
+ * <p>
+ * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ * <p>
+ *  
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：n = 3
+ * 输出：[
+ * "((()))",
+ * "(()())",
+ * "(())()",
+ * "()(())",
+ * "()()()"
+ * ]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/generate-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author zhanghailin
+ */
+public class LeetCode022_GenerateParenthesis {
+
+    List<String> res = new ArrayList<>();
+
+    public List<String> generateParenthesis(int n) {
+        dfs(n, n, "");
+        return res;
+    }
+
+    private void dfs(int left, int right, String curStr) {
+        if (left == 0 && right == 0) { // 左右括号都不剩余了，递归终止
+            res.add(curStr);
+            return;
+        }
+
+        if (left > 0) { // 如果左括号还剩余的话，可以拼接左括号
+            dfs(left - 1, right, curStr + "(");
+        }
+        if (right > left) { // 如果右括号剩余多于左括号剩余的话，可以拼接右括号
+            dfs(left, right - 1, curStr + ")");
+        }
+    }
+
+    public List<String> generateParenthesis2(int n) {
+        List<String> combinations = new ArrayList<>();
+        generateAll(new char[2 * n], 0, combinations);
+        return combinations;
+
+    }
+
+    public void generateAll(char[] current, int pos, List<String> result) {
+        if (pos == current.length) {
+            if (valid(current))
+                result.add(new String(current));
+        } else {
+            current[pos] = '(';
+            generateAll(current, pos+1, result);
+            current[pos] = ')';
+            generateAll(current, pos+1, result);
+        }
+    }
+
+
+    public boolean valid(char[] current) {
+        int balance = 0;
+        for (char c: current) {
+            if (c == '(') balance++;
+            else balance--;
+            if (balance < 0) return false;
+        }
+        return (balance == 0);
+    }
+
+
+    public static void main(String[] args) {
+        LeetCode022_GenerateParenthesis generateParenthesis = new LeetCode022_GenerateParenthesis();
+        generateParenthesis.generateParenthesis(3).forEach(System.out::println);
+    }
+}
