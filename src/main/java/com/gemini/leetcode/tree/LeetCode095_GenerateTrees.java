e015a61927a67ea3d32c8a886c0440ac71c09870
kaga <zhanghailin1995@gmail.com>
2020-04-20 09:18:14 +0800

    generate trees solution commit

---
+++ src/main/java/com/gemini/leetcode/tree/LeetCode095_GenerateTrees.java
@@ -0,0 +1,123 @@
+package com.gemini.leetcode.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * com.gemini.leetcode.tree.LeetCode095_GenerateTrees
+ * <p>
+ * 给定一个整数 n，生成所有由 1 ... n 为节点所组成的二叉搜索树。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 3
+ * 输出:
+ * [
+ *   [1,null,3,2],
+ *   [3,2,null,1],
+ *   [3,1,null,null,2],
+ *   [2,1,3],
+ *   [1,null,2,null,3]
+ * ]
+ * 解释:
+ * 以上的输出对应以下 5 种不同结构的二叉搜索树：
+ * <p>
+ * 1         3     3      2      1
+ * \       /     /      / \      \
+ * 3     2     1      1   3      2
+ * /     /       \                 \
+ * 2     1         2                 3
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/unique-binary-search-trees-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author zhanghailin
+ */
+public class LeetCode095_GenerateTrees {
+
+
+    public LinkedList<TreeNode> generate_trees(int start, int end) {
+        LinkedList<TreeNode> all_trees = new LinkedList<>();
+        //标识当前递归的节点没有左子树 ，end就是 上一层递归的i
+        if (start > end) {
+            all_trees.add(null);
+            return all_trees;
+        }
+
+        //尝试每个数字作为根节点
+        for (int i = start; i <= end; i++) {
+            // 得到所有可能的左子树
+            LinkedList<TreeNode> left_trees = generate_trees(start, i - 1);
+
+            // 得到所有可能的右子树
+            LinkedList<TreeNode> right_trees = generate_trees(i + 1, end);
+
+            // 以当前节点为根，左子树右子树两两组合
+            for (TreeNode l : left_trees) {
+                for (TreeNode r : right_trees) {
+                    TreeNode current_tree = new TreeNode(i);
+                    current_tree.left = l;
+                    current_tree.right = r;
+                    all_trees.add(current_tree);
+                }
+            }
+
+        }
+        return all_trees;
+    }
+
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new LinkedList<>();
+        }
+        return generate_trees(1, n);
+    }
+
+    /*************************************************分割线**************************************************************/
+
+    private TreeNode clone(TreeNode n, int offset) {
+        if (n == null) {
+            return null;
+        }
+        TreeNode node = new TreeNode(n.val + offset);
+        node.left = clone(n.left, offset);
+        node.right = clone(n.right, offset);
+        return node;
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<TreeNode> generateTrees2(int n) {
+        ArrayList<TreeNode>[] dp = new ArrayList[n + 1];
+        dp[0] = new ArrayList<>();
+        if (n == 0) {
+            return dp[0];
+        }
+        dp[0].add(null);
+        for (int len = 1; len <= n; len++) {
+            dp[len] = new ArrayList<>();
+            for (int root = 1; root <= len; root++) {
+                int left = root - 1;
+                int right = len = root;
+                for (TreeNode leftTree : dp[left]) {
+                    for (TreeNode rightTree : dp[right]) {
+                        TreeNode treeRoot = new TreeNode(root);
+                        treeRoot.left = leftTree;
+                        treeRoot.right = clone(rightTree,root);
+                        dp[len].add(treeRoot);
+                    }
+                }
+            }
+        }
+        return dp[n];
+    }
+
+    /*************************************************分割线**************************************************************/
+
+
+    public static void main(String[] args) {
+        LeetCode095_GenerateTrees leetCode095_generateTrees = new LeetCode095_GenerateTrees();
+        leetCode095_generateTrees.generateTrees(3);
+    }
+}
