ab27c0c6a6cb58cb710752d6a5e885421849866f
kaga <zhanghailin1995@gmail.com>
2020-04-21 20:37:29 +0800

    103 105 commit

---
+++ src/main/java/com/gemini/leetcode/tree/LeetCode103_ZigzagLevelOrder.java
@@ -0,0 +1,56 @@
+package com.gemini.leetcode.tree;
+
+import java.util.*;
+
+/**
+ * com.gemini.leetcode.tree.LeetCode103_ZigzagLevelOrder
+ *
+ * @author zhanghailin
+ */
+public class LeetCode103_ZigzagLevelOrder {
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        // 记录层数的奇偶性
+        int cnt = 0;
+        while (!q.isEmpty()) {
+            List<Integer> tmp = new ArrayList<>();
+            int len = q.size();
+            for (int i = 0; i < len; i++) {
+                TreeNode node = q.poll();
+                if (node == null) continue;
+                // 这里将层数的奇偶性做下判断
+                // 如果是偶数层就正向存储（以 0 为起始层）
+                if (cnt % 2 == 0) {
+                    tmp.add(node.val);
+                } else {
+                    // 奇数层则反向存储
+                    tmp.add(0, node.val);
+                }
+
+                if (node.left != null) {
+                    q.add(node.left);
+                }
+                if (node.right != null) {
+                    q.add(node.right);
+                }
+            }
+            cnt++;
+            ans.add(tmp);
+        }
+        return ans;
+
+
+    }
+
+    public static void main(String[] args) {
+        LeetCode103_ZigzagLevelOrder zigzagLevelOrder = new LeetCode103_ZigzagLevelOrder();
+        int[] arr = {1,2,3,4,5,6,7};
+        zigzagLevelOrder.zigzagLevelOrder(Tool.createTree(arr));
+    }
+}
---
+++ src/main/java/com/gemini/leetcode/tree/LeetCode105_BuildTree.java
@@ -0,0 +1,47 @@
+package com.gemini.leetcode.tree;
+
+import java.util.HashMap;
+
+/**
+ * com.gemini.leetcode.tree.LeetCode105_BuildTree
+ *
+ * @author zhanghailin
+ */
+public class LeetCode105_BuildTree {
+
+    int pre_idx = 0;
+    int[] preorder;
+    int[] inorder;
+
+    HashMap<Integer, Integer> idx_map = new HashMap<>();
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        this.preorder = preorder;
+        this.inorder = inorder;
+
+        // build a hashmap value -> its index
+        for (int idx = 0; idx < inorder.length; idx++) {
+            idx_map.put(inorder[idx], idx);
+        }
+        return helper(0, inorder.length);
+    }
+
+    public TreeNode helper(int in_left, int in_right) {
+        if (in_left == in_right)
+            return null;
+        int root_val = preorder[pre_idx];
+        TreeNode root = new TreeNode(root_val);
+
+        // 获取当前节点在中序遍历序列中的索引
+        int index = idx_map.get(root_val);
+
+        pre_idx++;
+
+        // 以[左起点,当前节点在中序遍历序列中的索引]递归构建左子树
+        root.left = helper(in_left, index);
+        // 以[当前节点在中序遍历序列中的索引,右起点]递归构建右子树
+        root.right = helper(index + 1, in_right);
+        return root;
+    }
+
+}
